acdbb6817593b5d664558ceefc1decfd7b229355
fatimohyetunde <fatimohyetundekuforiji@gmail.com>
2023-10-24 22:25:45 +0100

    Called the InvalidPurchaseException object in the TicketServiceImpl.java class
    Created logic to check the business case study

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,19 +1,68 @@
 package uk.gov.dwp.uc.pairtest;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.paymentgateway.TicketPaymentServiceImpl;
+import thirdparty.seatbooking.SeatReservationServiceImpl;
 import thirdparty.paymentgateway.TicketPaymentServiceImpl;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
-
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
 public class TicketServiceImpl extends TicketPaymentServiceImpl implements TicketService {
     /**
      * Should only have private methods other than the one below.
      */
+    final Map<TicketTypeRequest.Type, BigDecimal> ticketPrice = new HashMap<>();
+    final SeatReservationServiceImpl seatReservationService = new SeatReservationServiceImpl();
 
     @Override
-    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests)
+            throws InvalidPurchaseException {
 
+        validateNoOfTickets(ticketTypeRequests);
+        validateTicketPurchase(ticketTypeRequests);
+        makePayment(accountId, calculateAmountOfTicket(ticketTypeRequests));
+        seatReservationService.reserveSeat(accountId, calculateNoOfSeatsToBeAllocated(ticketTypeRequests));
     }
-   
+
+    private void validateNoOfTickets(TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        int ticketNo = Arrays.stream(ticketTypeRequests).mapToInt(TicketTypeRequest::getNoOfTickets).sum();
+        if (ticketNo > 20) {
+            throw new InvalidPurchaseException(
+                    "Number to tickets exceeds maximum no of tickets allowed which is twenty (20)");
+        }
+    }
+
+    private void validateTicketPurchase(TicketTypeRequest... typeRequests) throws InvalidPurchaseException {
+        Arrays.stream(typeRequests).filter(x -> x.getTicketType().equals(TicketTypeRequest.Type.ADULT)).findFirst()
+                .orElseThrow(() -> new InvalidPurchaseException(
+                        "You can not purchase a ticket without purchasing an Adult ticket"));
+    }
+
+    private int calculateAmountOfTicket(TicketTypeRequest... typeRequests) {
+        int ticketAmount = 0;
+        for (TicketTypeRequest ticketTypeRequest : typeRequests) {
+            int amount = getTicketPrice(ticketTypeRequest.getTicketType()) * ticketTypeRequest.getNoOfTickets();
+            ticketAmount += amount;
+        }
+        return ticketAmount;
+    }
+
+    private int calculateNoOfSeatsToBeAllocated(TicketTypeRequest... ticketTypeRequests) {
+        int noOfSeatsAllocated = 0;
+        for (TicketTypeRequest ticketTypeRequest : ticketTypeRequests) {
+            if (!ticketTypeRequest.getTicketType().equals(TicketTypeRequest.Type.INFANT))
+                noOfSeatsAllocated += ticketTypeRequest.getNoOfTickets();
+        }
+        return noOfSeatsAllocated;
+    }
+    
     private int getTicketPrice(TicketTypeRequest.Type ticketType) {
         int price;
         switch (ticketType) {
