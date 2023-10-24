42d076b30d3a33226e67ff7b83aae521b868952c
fatimohyetunde <fatimohyetundekuforiji@gmail.com>
2023-10-24 22:14:10 +0100

    Created ticketprice method in the ticketServiceImpl.java class

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,9 +1,10 @@
 package uk.gov.dwp.uc.pairtest;
 
+import thirdparty.paymentgateway.TicketPaymentServiceImpl;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
-public class TicketServiceImpl implements TicketService {
+public class TicketServiceImpl extends TicketPaymentServiceImpl implements TicketService {
     /**
      * Should only have private methods other than the one below.
      */
@@ -11,6 +12,24 @@ public class TicketServiceImpl implements TicketService {
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
 
+    }
+   
+    private int getTicketPrice(TicketTypeRequest.Type ticketType) {
+        int price;
+        switch (ticketType) {
+            case ADULT:
+                price = 20;
+                break;
+            case CHILD:
+                price = 10;
+                break;
+            case INFANT:
+                price = 0;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + ticketType);
+        }
+        return price;
     }
 
 }
