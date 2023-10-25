f8d56e8ab3a1ef76b894cf6091c45f25fdf83a2f
fatimohyetunde <fatimohyetundekuforiji@gmail.com>
2023-10-25 12:50:25 +0100

    Created testing method for cleaner code

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/exception/Main.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/exception/Main.java
@@ -5,16 +5,30 @@ import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 
 public class Main {
     public static void main(String[] args) {
-        //Instantiating the Ticket types
+        // Calling the method to test ticket types
+        testPurchaseTickets_NoAdultTicket();
+        testPurchaseTickets_InvalidNoOfTickets();
+    }
+
+    public static void testPurchaseTickets_InvalidNoOfTickets() throws InvalidPurchaseException {
+        // Instatiating the class
         TicketServiceImpl ticketService = new TicketServiceImpl();
         Long accountId = 123L;
-        TicketTypeRequest adultTicket = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
-        TicketTypeRequest childTicket = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1);
-        TicketTypeRequest infantTicket = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1);
+        TicketTypeRequest adultTicket = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 21); // More than the
+        
+        // New Object
+        ticketService.purchaseTickets(accountId, adultTicket);
+    }
 
-        //Testing all criteria to see if it meets the requirements
-        ticketService.purchaseTickets(accountId, adultTicket);      
-        // ticketService.purchaseTickets(accountId, childTicket);
-        // ticketService.purchaseTickets(accountId, infantTicket);
+    public static void testPurchaseTickets_NoAdultTicket() throws InvalidPurchaseException {
+        // Instatiating the class 
+        TicketServiceImpl ticketService = new TicketServiceImpl();
+        Long accountId = 123L;
+        TicketTypeRequest childTicket = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2);
+        TicketTypeRequest infantTicket = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 2);
+
+        // New objects
+        ticketService.purchaseTickets(accountId, childTicket);
+        ticketService.purchaseTickets(accountId, infantTicket);
     }
 }
